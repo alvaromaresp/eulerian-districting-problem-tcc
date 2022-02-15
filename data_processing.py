f96cba03032daf34e09988dae1be6d2b7537e338
Álvaro Espíndola <alvaro.espindola@estudante.ufla.br>
2022-02-14 21:52:58 -0300

    Better code structure in data_processing

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,5 @@
 import random
+from uuid import getnode
 from graph import Graph
 from edge import Edge
 from node import Node
@@ -7,47 +8,13 @@ def processFile(file, graph : Graph) -> None:
     for line in file:
         split = line.split(' ')
         
-        if (split[0] == "NODES"):
-            n_nodes = split[2].replace('\n','')
-            graph.setN_Nodes(int(n_nodes))
+        getNodeNumber(split, graph)
 
-        if (split[0] == "VEHICLES"):
-            vehicles = split[2].replace('\n', '')
-            graph.setVehicles(int(vehicles))
-        
-        if (split[0] == "CAPACITY"):
-            capacity = split[2].replace('\n', '')
-            graph.setCapacity(int(capacity))
-        
-        if (split[0][0] == '('):
-            nodes = split[0][1:-1].split(',')
-            cost : int = None
-            demand : int = 0
+        getVehiclesNumber(split, graph)
 
-            for edge_info in split:
-                if (edge_info == 'trav_cost' or edge_info == 'cost'):
-                    cost = getIntValueFromTitle(split, edge_info)
-                if (edge_info == 'demand'):
-                    demand = getIntValueFromTitle(split, edge_info)
-            
-            org = Node(int(nodes[0]) - 1)
-            dst = Node(int(nodes[1]) - 1)
+        getCapacity(split, graph)
 
-            edge = Edge(
-                len(graph.edges),
-                int(nodes[0]),
-                int(nodes[1]),
-                cost,
-                demand
-            )
-
-            #Are we considering arcs?
-            # org.addIncidentEdge(edge)
-            dst.addIncidentEdge(edge)
-
-            graph.addNode(org)
-            graph.addNode(dst)
-            graph.addEdge(edge)
+        getEdge(split, graph)
 
     graph.addDepot(0)
     graph.addDepot(1)
@@ -61,6 +28,52 @@ def processFile(file, graph : Graph) -> None:
     graph.setNodeParity()
     graph.setAllShortestPaths()
 
+def getNodeNumber(split, graph):
+    if (split[0] == "NODES"):
+        n_nodes = split[2].replace('\n','')
+        graph.setN_Nodes(int(n_nodes))
+
+def getVehiclesNumber(split, graph):
+    if (split[0] == "VEHICLES"):
+            vehicles = split[2].replace('\n', '')
+            graph.setVehicles(int(vehicles))
+
+def getCapacity(split, graph):
+    if (split[0] == "VEHICLES"):
+        vehicles = split[2].replace('\n', '')
+        graph.setVehicles(int(vehicles))
+
+def getEdge(split, graph):
+    if (split[0][0] == '('):
+        nodes = split[0][1:-1].split(',')
+        cost : int = None
+        demand : int = 0
+
+        for edge_info in split:
+            if (edge_info == 'trav_cost' or edge_info == 'cost'):
+                cost = getIntValueFromTitle(split, edge_info)
+            if (edge_info == 'demand'):
+                demand = getIntValueFromTitle(split, edge_info)
+        
+        org = Node(int(nodes[0]) - 1)
+        dst = Node(int(nodes[1]) - 1)
+
+        edge = Edge(
+            len(graph.edges),
+            int(nodes[0]),
+            int(nodes[1]),
+            cost,
+            demand
+        )
+
+        #Are we considering arcs?
+        org.addIncidentEdge(edge)
+        dst.addIncidentEdge(edge)
+
+        graph.addNode(org)
+        graph.addNode(dst)
+        graph.addEdge(edge)
+
 def getIntValueFromTitle(line : list[str], title : str) -> int:
     value_index = line.index(title) + 1
     value = line[value_index]
@@ -69,4 +82,5 @@ def getIntValueFromTitle(line : list[str], title : str) -> int:
         return int(value)
     except:
         return int(value)
+
 
